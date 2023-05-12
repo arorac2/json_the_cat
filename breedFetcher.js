5f10388338cc29ec8f1f4bd9a759eb7a8eaf862f
Chaahat <arorac2@mymacewan.ca>
2023-05-11 18:52:22 -0600

    refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
+
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Request failed:', error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error);
     } else {
-      const breed = data[0];
-      console.log(breed.description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(new Error(`Breed '${breedName}' not found.`));
+      } else {
+        const breed = data[0];
+        callback(null, breed.description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
